dba37bf02b5c0dfa09016196aab64117da41bd8e
halfstep <halfstep@gmail.com>
2021-01-01 11:28:57 -0600

    Adding Auth and merged Backend/frontend

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -6,7 +6,12 @@ const awsmobile = {
     "aws_appsync_graphqlEndpoint": "https://q2tsk6qte5exja5kr3rn44elei.appsync-api.us-east-1.amazonaws.com/graphql",
     "aws_appsync_region": "us-east-1",
     "aws_appsync_authenticationType": "API_KEY",
-    "aws_appsync_apiKey": "da2-gd2tqkqv4fhs7mx6jtyu4ourle"
+    "aws_appsync_apiKey": "da2-gd2tqkqv4fhs7mx6jtyu4ourle",
+    "aws_cognito_identity_pool_id": "us-east-1:5628746c-ada5-4e6d-8811-e3d21fc777e3",
+    "aws_cognito_region": "us-east-1",
+    "aws_user_pools_id": "us-east-1_SH6EwHcxR",
+    "aws_user_pools_web_client_id": "7hii61epv4eu8rg4r9erbobb2e",
+    "oauth": {}
 };
 
 
