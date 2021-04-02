3efd5a85997b5d59268ae30d961c5035ec0ce78a
halfstep <halfstep@gmail.com>
2021-04-01 23:50:00 -0500

    Update aws settings

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -3,10 +3,10 @@
 
 const awsmobile = {
     "aws_project_region": "us-east-1",
-    "aws_appsync_graphqlEndpoint": "https://q2tsk6qte5exja5kr3rn44elei.appsync-api.us-east-1.amazonaws.com/graphql",
+    "aws_appsync_graphqlEndpoint": "https://pa7oe6dwxvctroiw2eg2zngqte.appsync-api.us-east-1.amazonaws.com/graphql",
     "aws_appsync_region": "us-east-1",
     "aws_appsync_authenticationType": "API_KEY",
-    "aws_appsync_apiKey": "da2-gd2tqkqv4fhs7mx6jtyu4ourle",
+    "aws_appsync_apiKey": "da2-dpwuivo3hzh7vp4b4nrzbyuzue",
     "aws_cognito_identity_pool_id": "us-east-1:1846fb7c-303d-4840-b312-2e7a6f86b7a6",
     "aws_cognito_region": "us-east-1",
     "aws_user_pools_id": "us-east-1_1QFbaGqYX",
